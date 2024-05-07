85c98d67da693015ed5330b22ee56243c326237a
Ryuki <c0a2304235@edu.teu.ac.jp>
2024-05-07 15:15:57 +0900

    練習3

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -165,6 +165,8 @@ def main():
             if beam.rct.colliderect(bomb.rct):
                 beam = None
                 bomb = None
+                bird.change_img(6, screen)
+                pg.display.update()
                
 
         key_lst = pg.key.get_pressed()
